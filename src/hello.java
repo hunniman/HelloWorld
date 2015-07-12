1e881a701e38fd0dc284f5ecbc87dda873a8eb23
hunniman <pengweiyuang@gmail.com>
2015-07-12 18:38:15 +0800

    dev first commit

--- src/hello.java
+++ src/hello.java
@@ -3,7 +3,9 @@ public class hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.println("dddd");
+		System.out.println("dddd");
+		System.out.println("dddd");
+		System.out.println("dev brach");
 	}
 
 }
